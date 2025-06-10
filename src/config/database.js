4595e481c79a89dc0e09c70523956ce16170c794
rhydwharn <ridwan.abdulazeez1@gmail.com>
2025-06-10 14:39:39 +0100

    fixed-database-connection-on-local

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
@@ -13,11 +13,11 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        ca: fs.readFileSync(path.join(process.cwd(), 'src/config/ca.pem'))
-      }
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     ca: fs.readFileSync(path.join(process.cwd(), 'src/config/ca.pem'))
+    //   }
+    // },
     logging: false
   }
   
@@ -39,5 +39,5 @@ export const connectDB = async () => {
   }
 };
 
-// export default sequelize;
+export default sequelize;
 
